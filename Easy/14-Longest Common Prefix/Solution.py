50b0cb960090759a1aa18e9e2b60b7f328c6adfc
fruit-in <42092501+fruit-in@users.noreply.github.com>
2019-05-08 19:48:05 +0800

    add 1 solution of problem 14

---
+++ Easy/14-Longest Common Prefix/Solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ''
+        return self.findPrefix(strs[1:], strs[0])
+    
+    def findPrefix(self, strs: List[str], prefix: str) -> str:
+        for k, v in enumerate(strs):
+            if not v.startswith(prefix):
+                return self.findPrefix(strs[k:], prefix[:-1])
+        return prefix
