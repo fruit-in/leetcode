2706c069b583b4ee8d928b33db0ba608f0e73769
fruit-in <42092501+fruit-in@users.noreply.github.com>
2019-05-09 12:46:18 +0800

    rename all solutions of problem 14

--- Easy/14-Longest Common Prefix/Solution-Vertical scanning 2.py
+++ Easy/14-Longest Common Prefix/Solution-Vertical scanning 2.py
@@ -4,7 +4,7 @@ class Solution:
         if strs:
             for i, c in enumerate(strs[0]):
                 for s in strs[1:]:
-                    if i >= len(s) or s[i] != c:
+                    if i == len(s) or s[i] != c:
                         return prefix
                 prefix = strs[0][:i + 1]
         return prefix
