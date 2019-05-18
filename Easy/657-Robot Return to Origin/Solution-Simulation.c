eb97a01588f6adc5673d60d45c9062ce4834299d
fruit-in <42092501+fruit-in@users.noreply.github.com>
2019-05-18 13:34:00 +0800

    add problem 657

---
+++ Easy/657-Robot Return to Origin/Solution-Simulation.c
@@ -0,0 +1,13 @@
+bool judgeCircle(char * moves){
+    int x = 0;
+    int y = 0;
+    for(char* ch = moves; *ch != NULL; ch++) {
+        switch(*ch) {
+            case 'R': x++; break;
+            case 'L': x--; break;
+            case 'U': y++; break;
+            case 'D': y--; break;
+        }
+    }
+    return x == 0 && y == 0;
+}
