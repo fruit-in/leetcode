b76d33e899c19a61f7c3fc2df3f7d7eee15606f5
fruit-in <42092501+fruit-in@users.noreply.github.com>
2019-06-03 08:17:23 +0800

    add problem 559

---
+++ Easy/0559-Maximum Depth of N-ary Tree/Solution-Recursion.py
@@ -0,0 +1,16 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, children):
+        self.val = val
+        self.children = children
+"""
+
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        if not root:
+            return 0
+        if not root.children:
+            return 1
+        return max(map(self.maxDepth, root.children)) + 1
